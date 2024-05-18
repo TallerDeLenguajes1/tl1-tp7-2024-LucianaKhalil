463e8a63b99c09ed517735378d5e83fde24960de
Luciana <khalilmarialuciana@gmail.com>
2024-05-17 22:19:52 -0300

    segundo

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using Espacioalculadora;
+
+class Program{
+    static void Main(){
+        Console.WriteLine("hola mundo");
+    }
+}
---
+++ calculadora.cs
@@ -0,0 +1,27 @@
+namespace EspacioCalculadora{//organizador logico
+    public class Calculadora{
+        private double dato;
+
+        public double Resultado{//propiedad para acceder a el dato si esta privado
+            get => dato;            
+        }
+        public void sumar(double termino){
+            dato+=termino;
+        }
+         public void restar(double termino){
+            dato-=termino;
+        }
+        public void multiplicar(double termino){
+            dato*=termino;
+        }
+        public void dividir(double termino){
+        if(termino!=0){
+            dato/=termino;
+        }else{
+            Console.WriteLine("No se puede dividir por 0");
+        }
+        }
+}
+
+
+}
